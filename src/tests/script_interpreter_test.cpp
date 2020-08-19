8bd455dae31bc90c86a9f4a77c5df1e25615b2af
Roxxorfreak <roxxorfreak@miscalis.com>
2020-08-19 08:45:30 +0200

    Added test for TScriptInterpreter::ExecuteScriptAt()

--- src/tests/script_interpreter_test.cpp
+++ src/tests/script_interpreter_test.cpp
@@ -5,7 +5,7 @@
 TEST(TScriptInterpreter, LoadScript)
 {
     TString script;
-    TScriptInterpreter interpreter(TestDataFactory::GetScriptDir());
+    TScriptInterpreter interpreter(TestDataFactory::GetTestScriptDir());
 
     // Load script with invalid content
     script = "error1.hs";
@@ -13,9 +13,37 @@ TEST(TScriptInterpreter, LoadScript)
     ASSERT_STREQ("Invalid number <0x3t> passed as parameter 2", interpreter.GetLastError());
     ASSERT_STREQ("Checksum (XOR) with errors to test if error handling works", interpreter.ScriptDescription());
 
-    // Load script with valud content
+    // Load script with valid content
     script = "error2.hs";
     ASSERT_EQ(true, interpreter.LoadScript(script)) << interpreter.GetLastError();
     ASSERT_STREQ("", interpreter.GetLastError());
     ASSERT_STREQ("A plugin that has a very very very long title, so the creation of the Viewer menu can be tested to all extends - really all!", interpreter.ScriptDescription());
+}
+
+TEST(TScriptInterpreter, ExecuteScriptAt)
+{
+    TFile file(TestDataFactory::GetFilesDir() + "test.zip", true);
+    TString script;
+    TString buffer[100];
+    TScriptInterpreter interpreter(TestDataFactory::GetScriptDir());
+
+    // Invalid parameter tests
+    ASSERT_EQ(0, interpreter.ScriptExecuteAt(nullptr, 0, nullptr));
+    ASSERT_EQ(0, interpreter.ScriptExecuteAt(&file, 0, nullptr));
+    ASSERT_EQ(0, interpreter.ScriptExecuteAt(nullptr, 0, buffer));
+
+    // Load script for zip files
+    script = "zip.hs";
+    ASSERT_EQ(true, interpreter.LoadScript(script)) << interpreter.GetLastError();
+    ASSERT_STREQ("", interpreter.GetLastError());
+    ASSERT_STREQ("ZIP Archive", interpreter.ScriptDescription());
+
+    // Open the test data and execute the script
+    ASSERT_EQ(true, file.Open(TFileMode::READ));
+    ASSERT_EQ(5, interpreter.ScriptExecuteAt(&file, 0, buffer));
+    ASSERT_STREQ(" ERROR: Invalid cursor y position: Should be between 1 and 90, is 91", buffer[1].ToString());
+    file.Close();
+
+    // Debug
+    //for (const auto& val : buffer) if (!val.IsEmpty()) std::cout << val.ToString() << "\n";
 }
