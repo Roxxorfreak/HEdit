e8cab45dd876b8c07514a658e40b0d50ac654e6e
Roxxorfreak <roxxorfreak@miscalis.com>
2020-08-24 19:27:14 +0200

    Added test for TFile::Read()

--- src/tests/file_test.cpp
+++ src/tests/file_test.cpp
@@ -25,3 +25,31 @@ TEST(TFile, Open)
     // Delete test file
     ASSERT_EQ(0, _unlink(file_name.ToString())) << "Delete failed for <" << file_name.ToString() << ">";
 }
+
+TEST(TFile, Read)
+{
+    unsigned char buffer[64];
+    TFile file(TestDataFactory::GetFilesDir() + "test.zip", true);
+    TFile file2(TestDataFactory::GetFilesDir() + "test.zip", false);
+
+    // Read from closed file (with and without cache)
+    ASSERT_EQ(0u, file.Read(buffer, 4));
+    ASSERT_EQ(0u, file2.Read(buffer, 4));
+
+    // Read from openend file (with and without cache)
+    // With cache
+    ASSERT_EQ(true, file.Open(TFileMode::READ));
+    ASSERT_EQ(4u, file.Read(buffer, 4));
+    ASSERT_EQ('P', buffer[0]);
+    ASSERT_EQ('K', buffer[1]);
+    ASSERT_EQ(0x03, buffer[2]);
+    ASSERT_EQ(0x04, buffer[3]);
+    // No cache
+    ASSERT_EQ(true, file2.Open(TFileMode::READ));
+    ASSERT_EQ(4u, file2.Read(buffer, 4));
+    ASSERT_EQ(4u, file2.Read(buffer, 4));
+    ASSERT_EQ(0x0A, buffer[0]);
+    ASSERT_EQ(0x00, buffer[1]);
+    ASSERT_EQ(0x00, buffer[2]);
+    ASSERT_EQ(0x00, buffer[3]);
+}
